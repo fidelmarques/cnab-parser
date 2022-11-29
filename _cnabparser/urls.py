761ab1a40a8ec6e476db819bd04ea1aae6c95a7e
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-11-29 04:04:15 -0300

    feat: initial commit

---
+++ _cnabparser/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("file.urls")),
+    path("api/", include("transactions.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+]
